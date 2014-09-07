829ba370b8bca3b2c28e4df528972caa191986be
Kevin Me <chekmanirov@gmail.com>
2014-09-07 00:08:58 -0400

    favicon

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
+var favicon  = require('serve-favicon');
 var passport = require('passport');
 var flash 	 = require('connect-flash');
 
@@ -23,7 +24,8 @@ mongoose.connect(configDB.url); // connect to our database
  require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
- app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
