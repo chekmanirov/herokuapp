bf916ad46b460955ca119713d5578894079f7d7b
Kevin Me <chekmanirov@gmail.com>
2014-09-11 14:12:58 -0400

    preliminary scaffolding for dynamic rendering

---
+++ app/models/profile.js
@@ -0,0 +1,47 @@
+/*
+var mongoose = require('mongoose');
+
+var profileSchema = mongoose.Schema({
+
+    profile            : {
+        id           : String,
+        url          : String
+    },
+    personal         : {
+        name         : String,
+        address      : String,
+        city         : String,
+        stateProvince: String,
+        zipPostal    : String,
+        country      : String,
+        phone        : String,
+        fax          : String,
+        email:       : String,
+        linkedin     : {
+        	url      : String,
+        	name     : String
+        },
+        github       : {
+        	url      : String,
+        	name     : String
+        },
+        institution  : {
+        	name:    : String,
+        	occupation : String,
+        	program  : String,
+        	timeline : String,
+        	specialization : String
+        }
+        
+    },
+    sections         : {
+        id           : String,
+        token        : String,
+        displayName  : String,
+        username     : String
+    }
+
+});
+
+module.exports = mongoose.model('Profile', profileSchema);
+*/
---
+++ app/models/section.js
@@ -0,0 +1,30 @@
+	var mongoose = require('mongoose');
+
+var sectionSchema = mongoose.Schema({
+        name         : String,
+        address      : String,
+        city         : String,
+        stateProvince: String,
+        zipPostal    : String,
+        country      : String,
+        phone        : String,
+        fax          : String,
+        email:       : String,
+        linkedin     : {
+        	url      : String,
+        	name     : String
+        },
+        github       : {
+        	url      : String,
+        	name     : String
+        },
+        institution  : {
+        	name:    : String,
+        	occupation : String,
+        	program  : String,
+        	timeline : String,
+        	specialization : String
+        }
+        });
+
+module.exports = mongoose.model('Section', sectionSchema);
